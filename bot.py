6e97259678e3eb56699b85384f3ca874da8b6c08
rainbowsock <50048459+rainbowsock@users.noreply.github.com>
2019-06-27 15:44:23 +0300

    Add bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,10 @@ bot = telebot.TeleBot('625199341:AAGuM7KLOKHGUHKv1Xo0-htS8pDu0IUedic')
 
 @bot.message_handler(comands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Приветствую')	
+    bot.send_message(message.chat.id, 'Приветствую')
+if message.text == 'Привет': 
+    bot.send_message(message.chat.id, 'Привет, мой господин') 
+elif message.text == 'Пока': 
+    bot.send_message(message.chat.id, 'Прощай, господин') 	
 
 bot.polling()
